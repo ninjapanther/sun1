4419cbc9bd6c521dba05dd3cf0bb6db5672f0a21
blog <rubygig@yahoo.com>
2017-08-19 15:03:33 +0530

    home index

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ git_source(:github) do |repo_name|
 end
 
 gem 'carrierwave'
+#gem 'mini_magick', '3.8.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.0.5'
 # Use sqlite3 as the database for Active Record
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
-
+#version :thumb do
+ # process :resize_to_fill => [50, 50]
+#end
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
